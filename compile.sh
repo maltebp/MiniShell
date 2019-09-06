d8026c395e45e45fc38df70d15367e57dddb0468
maltebp <maltebp94@gmail.com>
2019-09-06 18:34:30 +0200

    ??

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo rm -rf $HOME/MiniShell && cp -avr $WIN/MiniShell $HOME/MiniShell && cd $HOME/MiniShell && make clean && make && ./minishell && cd ..
+sudo rm -rf $HOME/MiniShell && cp -avr $WIN/MiniShell $HOME/MiniShell && cd $HOME/MiniShell && make clean && make  && ./minishell && cd ..
+# Removes the MiniShell directory from Ubuntu, copies the directory from Windows, runs the make cleaning and make, and runs the projects
