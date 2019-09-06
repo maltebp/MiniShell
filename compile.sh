64f267f3034d1635d11b0b0a92f51ada2eb0dba7
maltebp <maltebp94@gmail.com>
2019-09-06 17:04:49 +0200

    Added c++ (strings) compliant wrapper function for execv

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -Wall -o $HOME/minishell $WIN/MiniShell/application.cpp && g++ -Wall -o $HOME/hello $WIN/MiniShell/hello.cpp && $HOME/minishell
+g++ -Wall -o $HOME/minishell $WIN/MiniShell/application.cpp && $HOME/minishell
--- exec.cpp
+++ exec.cpp
@@ -1,9 +1,48 @@
 
 #include <iostream>
 #include <vector>
+#include <unistd.h>
+#include <sstream>
 
 using namespace std;
 
-void exec( string filename, const vector<string> &args){
-    cout<<"Exec "<<filename<<endl;
+
+
+int execvString( const string &path, const vector<string> &args){
+
+    char* c_arr[args.size()+1];
+
+    for( unsigned int i=0; i<args.size(); i++){
+        char* str = new char[(args[i]).size()+1];
+        strcpy(str, (args[i]).c_str());
+        c_arr[i] = str;
+    }
+
+    // Adding the null pointer to end of argument list to conform to execv
+    c_arr[args.size()] = NULL;
+
+    return execv(path.c_str(), c_arr);
 }
+
+
+void exec( string filename, const vector<string> &args){
+    
+    
+    cout<<"Exec "<<filename<<endl;
+
+    int result = execvString(filename, args);
+
+    cout<<"Execv Res: "<<result<<endl;
+
+    string errormsg(strerror(errno));
+
+    cout<<"Error running program: "<<errormsg<<endl;
+
+    
+
+    cout<<"Finished"<<endl;
+
+    
+}
+
+
