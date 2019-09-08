d5ea4ccc44d197f2edbc2236a8635d0929e591c7
maltebp <maltebp94@gmail.com>
2019-09-08 12:22:44 +0200

    Added help command

--- main.cpp
+++ main.cpp
@@ -15,6 +15,16 @@ using namespace std;
 #define PROMPT "MiniShell:"
 
 
+void help(){
+    cout<<"\nMiniShell is a small application implementing basic functionality:\n"<<
+            "  cd [DIR]             \t Change working directory. '..' goes one directory back, and 'cd' goes to home directory.\n"<<
+            "  dir/pwd              \t Prints current working directory.\n"
+            "  [FILE] [ARG1] ..     \t Executes selected file if it's executable, including the given arguments.\n"<<
+            "  [FILE1] | [FILE2]    \t Pipes the output of file 1 into the output of filee 2.\n"<<
+            endl;
+}
+
+
 // Sort arguments and put them into vector (list)
 const vector<string>& getArguments(string input){
     vector<string> *args;
@@ -53,11 +63,17 @@ string readInput(){
 
 
 int main(){
-    while(1){ 
+    
+    cout<< "\nMiniShell started! Type 'help' to see available functionality"<<endl;;
+
+    while(1){
         string input = readInput();
         const vector<string> args = getArguments(input);
         if(args.size() > 0){
-            if( !runDirCommand(args) ){
+            if( args[0] == "help") 
+                help();
+            else if( !runDirCommand(args) ){
+                
                 if( isPipeCommand(args) )
                     forkCommandPipe(args);
                     
