78dc6e36573713716922c5df4acbcb32a41bd262
maltebp <maltebp94@gmail.com>
2019-09-08 12:25:21 +0200

    Removed unversioned files

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo rm -rf $HOME/MiniShell && cp -avr $WIN/MiniShell $HOME/MiniShell && cd $HOME/MiniShell && make clean && make && ./minishell && cd ..
-# Removes the MiniShell directory from Ubuntu, copies the directory from Windows, runs the make cleaning and make, and runs the projects
